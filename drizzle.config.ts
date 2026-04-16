a37acb20ebb4c474cb5e2c517299a08f7f8df9af
Derick <derick_618@hotmail.com>
2026-04-16 17:09:05 +0800

    Fix: drizzle issue

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./db/schema.ts",
   out: "./drizzle",
-  driver: "turso",   // This tells the older version how to handle the credentials
+  dialect: "turso", // Use "turso" here
   dbCredentials: {
     url: process.env.TURSO_CONNECTION_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN!,
